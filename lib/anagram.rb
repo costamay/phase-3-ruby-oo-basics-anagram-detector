15280561232c67497d56f56b6342a3bb80b45420
costamay <titusouko@gmail.com>
2022-05-17 17:11:47 +0300

    commit tree 86acb2351ab564cb4334c450fff59267f81e00b6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_reader :word
+    def initialize(word="titus")
+        @word = word
+    end
+    def match(list_of_words)
+        list_of_words.select {|word| word.chars.sort == @word.chars.sort}      
+    end
+end
